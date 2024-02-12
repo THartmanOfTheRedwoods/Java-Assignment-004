883bcc6d9820ad8f8e5095b42a602d18e84a48a0
Shuulker <JosephSparrowRoberts26@gmail.com>
2024-02-11 19:19:37 -0800

    Completed Java Assignment 004

---
+++ src/multadd.java
@@ -0,0 +1,18 @@
+public class multadd {
+
+    public static double expSum(double x) {
+        return multadd(x, Math.exp(-x), Math.sqrt(1 - Math.exp(-x)));
+    }
+
+    public static double multadd(double a, double b, double c) {
+        return a + b * c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multadd(1.0,2.0,3.0));
+        System.out.println(multadd(Math.sin(Math.PI/4),(Math.cos(Math.PI/4.0))/2.0, 1.0));
+        System.out.println(multadd(Math.log(10.0),Math.log(20.0),1.0));
+
+        System.out.println(expSum(1.0));
+    }
+}
